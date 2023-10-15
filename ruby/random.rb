a0fd8e331d91e6e3d850f4404e420de5ff6b36d1
satohirok <hiroki_g0421@yahoo.co.jp>
2023-10-16 08:19:54 +0900

    取得人数修正

--- ruby/random.rb
+++ ruby/random.rb
@@ -1,10 +1,34 @@
-members = ['A','B','C','D','E','F'].shuffle!
+members = ['A','B','C','D','E','F']
 
 
-p '3人と3人にランダムに分ける'
-p members[0..2].sort!
-p members[3..5].sort!
+random_num1 = [2,3,4].sample
 
-p '2人と4人にランダムに分ける'
-p members[0..1].sort!
-p members[2..5].sort!
+
+group1 = members.sample(random_num1).sort
+group2 = members - group1
+
+p group1.sort
+p group2.sort
+
+
+
+
+
+# p pick_num2 = [5 - pick_num1]
+
+
+# group1 = members[0..pick_num1]
+# group2 = members[]
+
+
+
+
+# p group1.sort
+# p group2.sort
+
+# p members[0..2].sort!
+# p members[3..5].sort!
+
+
+# p members[0..1].sort!
+# p members[2..5].sort!
