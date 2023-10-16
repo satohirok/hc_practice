03f26c46f114d9f471ed28e714eaeb5a3fc624fb
satohirok <hiroki_g0421@yahoo.co.jp>
2023-10-16 12:09:59 +0900

    コメントアウト削除

--- ruby/random.rb
+++ ruby/random.rb
@@ -11,24 +11,3 @@ p group1.sort
 p group2.sort
 
 
-
-
-
-# p pick_num2 = [5 - pick_num1]
-
-
-# group1 = members[0..pick_num1]
-# group2 = members[]
-
-
-
-
-# p group1.sort
-# p group2.sort
-
-# p members[0..2].sort!
-# p members[3..5].sort!
-
-
-# p members[0..1].sort!
-# p members[2..5].sort!
