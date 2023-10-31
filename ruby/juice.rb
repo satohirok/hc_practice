b2c59649259992d1c711385d17b5a6f8abc04010
satohirok <hiroki_g0421@yahoo.co.jp>
2023-11-01 07:18:26 +0900

    juiceのレシーバーメソッドをattr_readerで代替

--- ruby/juice.rb
+++ ruby/juice.rb
@@ -1,15 +1,7 @@
 class Juice
-  attr_reader :name
+  attr_reader :name, :price
   def initialize(name,price)
       @name = name
       @price = price
-  end
-
-  def name
-    @name
-  end
-
-  def price
-    @price
   end
 end
